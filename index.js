1f2e77aec8ef2a219f3d616f23eac5d8c61c79bf
Filipe Laitenberger <f.laitenberger@gmail.com>
2020-10-05 09:52:15 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { Text, Float, Checkbox, Select, Url } = require('@keystonejs/fields');
+const { CloudinaryAdapter } = require('@keystonejs/file-adapters');
+const { CloudinaryImage } = require('@keystonejs/fields-cloudinary-image');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { NuxtApp } = require('@keystonejs/app-nuxt');
+
+require('dotenv').config();
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'FindMyWorkout';
+const adapterConfig = { mongoUri: 'mongodb://localhost/' + process.env.MONGODB };
+
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+});
+
+const fileAdapter = new CloudinaryAdapter({
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: 'find-my-workout',
+});
+
+
+keystone.createList('Program', {
+  schemaDoc: 'A collection of workout programs',
+  fields: {
+    name: { type: Text, schemaDoc: 'The name of the program', isRequired: true },
+
+    link: { type: Url, schemaDoc: 'The link to the program'},
+
+    image: { type: CloudinaryImage, adapter: fileAdapter, schemaDoc: 'The image of the program', },
+
+    price: { type: Float, schemaDoc: 'How much does it cost?' },
+    priceType: { type: Select, options: ['per day', 'per week', 'per month', 'per year', 'one time payment'], dataType: 'string', schemaDoc: 'What kind of price?' },
+
+    // type
+    weightlifting: { type: Checkbox, schemaDoc: 'Does it include weightlifting?' },
+    gymnastics: { type: Checkbox, schemaDoc: 'Does it include gymnastics?' },
+    metcon: { type: Checkbox, schemaDoc: 'Does it include metabolic conditioning?' },
+    engineRunning: { type: Checkbox, schemaDoc: 'Does it include engine running?' },
+    strongman: { type: Checkbox, schemaDoc: 'Does it include strongman training?' },
+
+    // goal
+    competitions: { type: Checkbox, schemaDoc: 'Can you train for competitions with it?' },
+    opens: { type: Checkbox, schemaDoc: 'Can you train for the crossfit opens with it?' },
+    games: { type: Checkbox, schemaDoc: 'Can you train for the crossfit games with it?' },
+    fitness: { type: Checkbox, schemaDoc: 'Can you train for fitness with it?' },
+    weightloss: { type: Checkbox, schemaDoc: 'Can you lose weight with it?' },
+    musclegain: { type: Checkbox, schemaDoc: 'Can you gain muscle with it?' },
+
+    level: { type: Select, options: ['Beginner', 'Intermediate', 'Advanced', 'Elite'], dataType: 'string', schemaDoc: 'What level is the program?' },
+
+    // language
+    english: { type: Checkbox, schemaDoc: 'Is it available in English?' },
+    german: { type: Checkbox, schemaDoc: 'Is it available in German?' },
+    spanish: { type: Checkbox, schemaDoc: 'Is it available in Spanish?' },
+
+    // extras
+    comesWithApp: { type: Checkbox, schemaDoc: 'Does it include an app?' },
+    personalized: { type: Checkbox, schemaDoc: 'Is it personalized for the athlete?' },
+    videoCoaching: { type: Checkbox, schemaDoc: 'Does it include video coaching?' },
+
+    trainingDaysPerWeek: { type: Float, schemaDoc: 'How many training days per week?' },
+    actRecDaysPerWeek: { type: Float, schemaDoc: 'How many active recovery days per week?' },
+    hoursPerDay: { type: Float, schemaDoc: 'How many hours does a training take on average?' }
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({ name: PROJECT_NAME }),
+    new NuxtApp({
+      srcDir: 'src',
+      buildDir: 'dist',
+      plugins: [
+        '~/plugins/vue-formulate',
+        '~/plugins/vue-click-outside'
+      ]
+    }),
+  ],
+};
---
+++ src/components/cSearchInput.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="c-search-input" :class="{focus: isFocused}">
+    <transition name="fade">
+      <div class="tiny-label" v-if="value.length">Search</div>
+    </transition>
+    <i class="las la-search"></i>
+    <input
+        type="text"
+        placeholder="Search"
+        :value="value"
+        @focus="isFocused = true"
+        @blur="isFocused = false"
+        @input="$emit('input', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isFocused: false,
+    };
+  },
+
+  props: {
+    value: {
+      type: String,
+      default: ""
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.c-search-input {
+  background: #FFF;
+  padding: 8px 16px;
+  border-radius: 10px;
+  box-shadow: 0 4px 10px 0 rgba(128,128,128,0.1);
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  border: 3px solid transparent;
+  position: relative;
+  width: 100%;
+
+  &:hover, &.focus {
+    border: 3px solid #4D4D4D;
+    cursor: pointer;
+  }
+
+  i {
+    margin-right: 12px;
+    font-size: 30px;
+  }
+
+  .tiny-label {
+    position: absolute;
+    font-size: 10px;
+    top: 4px;
+    left: 58px;
+    color: #4D4D4D;
+  }
+
+  input {
+    border: transparent;
+    padding: 12px;
+    margin-left: -56px;
+    margin-right: -8px;
+    margin-top: -8px;
+    margin-bottom: -8px;
+    padding-left: 56px;
+    background: transparent;
+    font-size: 16px;
+    font-family: "Roboto", "Open Sans", "Helvetica Neue", "Arial";
+    color: #4D4D4D;
+    width: 100%;
+
+    &:focus {
+      outline: none !important;
+    }
+  }
+}
+</style>

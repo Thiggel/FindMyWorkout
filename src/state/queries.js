458f1df2047622d1d5f2a318f8b3152157362c13
Filipe Laitenberger <f.laitenberger@gmail.com>
2020-10-24 12:31:17 +0200

    most filters work now

---
+++ src/state/queries.js
@@ -0,0 +1,71 @@
+exports.GET_DATA = `
+  query GetData {
+    allPrograms {
+      id
+      name
+      link
+      image {
+        filename
+        publicUrlTransformed(transformation: { width: "400", crop: "limit" })
+      }
+      price
+      priceType
+      currency
+
+      trainingDaysPerWeek
+      actRecDaysPerWeek
+      hoursPerDay
+
+      payment { name }
+      trainingTypes { name }
+      goal { name }
+      level { name }
+      languages { name }
+      extras { name }
+      ages { name }
+    }
+
+    allPaymentTypes {
+      name
+    }
+
+    allTrainingTypes {
+      name
+    }
+
+    allGoals {
+      name
+    }
+
+    allLevels {
+      name
+    }
+
+    allLanguages {
+      name
+    }
+
+    allAges {
+      name
+    }
+
+    allExtras {
+      name
+    }
+  }
+`;
+
+exports.graphql = function(query, variables = {}) {
+    return fetch(process.env.BASE_URL + '/admin/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            variables,
+            query,
+        }),
+    }).then(function(result) {
+        return result.json();
+    });
+};
